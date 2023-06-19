a6ad11c00b37189972cc5627a6d150cd55a9a942
formateur1 <enseignant.formateur@protonmail.com>
2023-06-19 15:06:53 +0200

    ex 2 : delete produit

--- src/main/java/com/inti/controller/ProduitController.java
+++ src/main/java/com/inti/controller/ProduitController.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -66,6 +67,14 @@ public class ProduitController {
 		m.addAttribute("listeP", ips.getProduits());
 		
 		return "produits";
+	}
+	
+	@GetMapping("deleteProduit/{id}")
+	public String deleteP(@PathVariable("id") int id)
+	{
+		ips.deleteProduit(id);
+		
+		return "redirect:/produit/produits";
 	}
 
 }
